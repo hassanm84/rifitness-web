96f527f826f62fa78f644254563e6ad8a4257b9b
ingenieur84 <m.h.masood@live.co.uk>
2021-01-12 05:39:14 +0000

    chore(app.js): included ipv4 address argument in listen function.

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.use(function (req, res, next) {
 
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, '127.0.0.1' function(){
+  console.log("Server started 🚀on port 3000.");
 });
